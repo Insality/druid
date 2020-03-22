278a61b99db195b1ce1ad5e05d12f4745e12fc4b
Insality <insality@gmail.com>
2020-03-22 11:45:54 +0300

    Default style as druid default style (ha-ha)

--- druid/system/settings.lua
+++ druid/system/settings.lua
@@ -2,9 +2,11 @@
 -- @module settings
 -- @local
 
+local default_style = require("druid.styles.default.style")
+
 local M = {}
 
-M.default_style = nil
+M.default_style = default_style
 
 function M.get_text(name)
 	return "[Druid]: locales not inited"
