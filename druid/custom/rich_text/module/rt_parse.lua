2a7d25500e08bda2c0a9f26e45dd8c45a827a6da
Insality <insality@gmail.com>
2025-10-14 00:34:44 +0300

    Solve #329 Allow numeric characters in RichText tags

--- druid/custom/rich_text/module/rt_parse.lua
+++ druid/custom/rich_text/module/rt_parse.lua
@@ -159,7 +159,7 @@ function M.parse(text, default_settings, style)
 		end
 
 		-- parse the tag, split into name and optional parameters
-		local endtag, name, params, empty = tag:match("<(/?)([%a_]+)=?(%S-)(/?)>")
+		local endtag, name, params, empty = tag:match("<(/?)([%w_]+)=?(%S-)(/?)>")
 
 		local is_endtag = endtag == "/"
 		local is_empty = empty == "/"
